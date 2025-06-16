9355176701001f110c9fd087b2773fac8b88f869
RaviTwari03 <ravirkt0@gmail.com>
2025-06-16 14:22:33 +0530

    Initial Commit

---
+++ Indian PIN code/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Indian PIN code
+//
+//  Created by Ravi Tiwari on 16/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Indian PIN code/Indian_PIN_codeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Indian_PIN_codeApp.swift
+//  Indian PIN code
+//
+//  Created by Ravi Tiwari on 16/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Indian_PIN_codeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
